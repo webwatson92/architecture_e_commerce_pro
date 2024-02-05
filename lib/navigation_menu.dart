678a0d9fca2694d89adb04a5809c6be6e528fef1
webwatson92 <webwatson92@gmail.com>
2024-02-05 17:51:30 +0000

    finalisation du bottomNavigation

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,4 +1,8 @@
+import 'package:e_commerce_pro/screens/home_screen.dart';
+import 'package:e_commerce_pro/utils/constants/colors.dart';
+import 'package:e_commerce_pro/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 class NavigationMenu extends StatelessWidget {
@@ -6,15 +10,38 @@ class NavigationMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final darkMode = THelperFunctions.isDarkMode(context);
+
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        destinations: const [
-          NavigationDestination(icon: Icon(Iconsax.home), label: "Home"),
-          NavigationDestination(icon: Icon(Iconsax.shop), label: "Store"),
-          NavigationDestination(icon: Icon(Iconsax.heart), label: "Wishlist"),
-          NavigationDestination(icon: Icon(Iconsax.user), label: "Profile"),
-        ],
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) => controller.selectedIndex.value = index,
+          backgroundColor: darkMode ? TColors.black : TColors.white,
+          indicatorColor: darkMode ? TColors.white.withOpacity(0.1) : TColors.black.withOpacity(0.1),
+
+          destinations: const [
+            NavigationDestination(icon: Icon(Iconsax.home), label: "Home"),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: "Store"),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: "Wishlist"),
+            NavigationDestination(icon: Icon(Iconsax.user), label: "Profile"),
+          ],
+        ),
       ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
     );
   }
+}
+
+class NavigationController extends GetxController{
+  final Rx<int> selectedIndex = 0.obs;
+  final screens = [
+    const HomeScreen(),
+    Container(color: Colors.purple),
+    Container(color: Colors.orange),
+    Container(color: Colors.yellow),
+  ];
 }
